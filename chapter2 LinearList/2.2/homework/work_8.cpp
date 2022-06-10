9e04aea99eb19afca7efdf41c314fd5a0e193fdb
AnQi1997 <2361067080@qq.com>
2022-06-10 12:28:36 +0800

    main

--- chapter2 LinearList/2.2/homework/work_8.cpp
+++ chapter2 LinearList/2.2/homework/work_8.cpp
@@ -4,7 +4,9 @@
 //  8.已知在一维数组A[m+n]中依次存放两个线性表(a1, a2, a3, ... , am)和(b1, b2, b3, ... , bn).
 //   试编写一个函数,将数组中两个顺序表的位置互换,
 //即将(b1, b2, b3, ... , bn)放在(a1, a2, a3, ... , am)前面
+
 typedef int DataType;
+
 void Reverse(DataType A[], int left, int right, int arraySize) {
     //判断范围合法
     if (left >= right || right >= arraySize)
@@ -19,8 +21,8 @@ void Reverse(DataType A[], int left, int right, int arraySize) {
     // mid - left = 1 - 0 = 1
 
     // i = 0:
-    // temp = A[0 + 0] 
-    // A[0 + 0] = A[2 - 0] 
+    // temp = A[0 + 0]
+    // A[0 + 0] = A[2 - 0]
     // A[2 - 0] = temp
     // A[0] 与 A[2]互换
 
@@ -29,11 +31,11 @@ void Reverse(DataType A[], int left, int right, int arraySize) {
     // A[0 + 1] = A[2 - 1];
     // A[2 - 1] = A[0 + 1];
     // A[1]不动
-// ---------------------------------------------------------
+    // ---------------------------------------------------------
     // 下标:3 4 5 6 7
     // 元素:2 4 6 8 10
-    
-    // left = 3 
+
+    // left = 3
     // right = 7
     // mid = (3 + 7) / 2 = 5;
     // mid - left = 5 - 3 = 2;
@@ -50,6 +52,14 @@ void Reverse(DataType A[], int left, int right, int arraySize) {
     }
 }
 
+/**
+ * @brief
+ *
+ * @param A 数组名
+ * @param m 前面有m个
+ * @param n 后面有n个
+ * @param arraySize
+ */
 void Exchange(DataType A[], int m, int n, int arraySize) {
     //数组A[m+n]中,
     // 从0到m-1存放顺序表(a1, a2, a3, ... , am),
