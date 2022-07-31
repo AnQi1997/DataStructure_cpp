8d0540d6bca7240cab4b8ad5a2eb12ed15e462b8
AnQi1997 <2361067080@qq.com>
2022-07-31 17:04:43 +0800

    更新

--- chapter3 Stack&Queue includeWork&Answer/3.3.3 斐波那契数列/Fibonacci.cpp
+++ chapter3 Stack&Queue includeWork&Answer/3.3.3 斐波那契数列/Fibonacci.cpp
@@ -1,20 +1,19 @@
 /*
  * @Author: 2361067080@qq.com
- * @Date: 2022-06-27 15:43:12
- * @Description: 斐波那契数列问题
+ * @Date: 2022-07-29 16:53:31
+ * @Description: 斐波那契数列问题练习
+ * 爬楼梯
  */
 #include <stdio.h>
 #include <stdlib.h>
 
-// 0 1 1 2 3 5
-// f(n) = f(n-1) + f(n-2)
 int Fib(int n) {
     if (0 == n)
         return 0;
     else if (1 == n)
         return 1;
     else
-        return Fib(n - 2) + Fib(n - 1);
+        return Fib(n - 1) + Fib(n - 2);
 }
 
 int main() {
--- chapter5 BinaryTree/4.3.1 二叉树的遍历/stack.cpp
+++ chapter5 BinaryTree/4.3.1 二叉树的遍历/stack.cpp
@@ -1,39 +1,41 @@
-/*
- * @Author: 2361067080@qq.com
- * @Date: 2022-05-15 17:43:12
- * @Description:
- */
 #include "function.h"
 
-void InitStack(SqStack& S) {
-    S.top = -1;
+
+void InitStack(SqStack &S)
+{
+	S.top=-1;
 }
 
-bool StackEmpty(SqStack& S) {
-    if (S.top == -1)
-        return true;
-    else
-        return false;
+bool StackEmpty(SqStack &S)
+{
+	if(S.top==-1)
+		return true;
+	else
+		return false;
 }
 //入栈
-bool Push(SqStack& S, ElemType x) {
-    if (S.top == MaxSize - 1) {
-        return false;
-    }
-    S.data[++S.top] = x;
-    return true;
+bool Push(SqStack &S,ElemType x)
+{
+	if(S.top==MaxSize-1)
+	{
+		return false;
+	}
+	S.data[++S.top]=x;
+	return true;
 }
 //出栈
-bool Pop(SqStack& S, ElemType& x) {
-    if (-1 == S.top)
-        return false;
-    x = S.data[S.top--];
-    return true;
+bool Pop(SqStack &S,ElemType &x)
+{
+	if(-1==S.top)
+		return false;
+	x=S.data[S.top--];
+	return true;
 }
 //读取栈顶元素
-bool GetTop(SqStack& S, ElemType& x) {
-    if (-1 == S.top)
-        return false;
-    x = S.data[S.top];
-    return true;
+bool GetTop(SqStack &S,ElemType &x)
+{
+	if(-1==S.top)
+		return false;
+	x=S.data[S.top];
+	return true;
 }
