06bb1deb9bcc1e923047fdae9c3cd9fe6677e162
AnQi1997 <2361067080@qq.com>
2022-09-27 20:33:50 +0800

    回顾单链表

--- chapter2 LinearList/2.3 LinkedList/20220927restart.cpp
+++ chapter2 LinearList/2.3 LinkedList/20220927restart.cpp
@@ -1,7 +1,7 @@
 /*
  * @Author: 2361067080@qq.com
- * @Date: 2022-07-22 16:09:40
- * @Description: 链表
+ * @Date: 2022-09-27 17:53:52
+ * @Description: 重启单链表
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,24 +13,15 @@ typedef struct LNode {
     struct LNode* next;
 } LNode, *LinkList;
 
-void PrintList(LinkList L) {
-    L = L->next;
-    while (L) {
-        printf("%6d", L->data);
-        L = L->next;
-    }
-    printf("\n");
-}
-
-//头插法建立单链表
+//头插法
 LinkList ListHeadInsert(LinkList& L) {
     L = (LinkList)malloc(sizeof(LNode));
     L->next = NULL;
 
     LNode* s;
     int x;
-
     scanf("%d", &x);
+
     while (x != 9999) {
         s = (LNode*)malloc(sizeof(LNode));
         s->data = x;
@@ -40,15 +31,12 @@ LinkList ListHeadInsert(LinkList& L) {
 
         scanf("%d", &x);
     }
-
     return L;
 }
 
-//尾插法建立单链表
+//尾插法
 LinkList ListTailInsert(LinkList& L) {
     L = (LinkList)malloc(sizeof(LNode));
-    // L->next = NULL;
-
     LNode *s, *r = L;
 
     int x;
@@ -70,13 +58,13 @@ LinkList ListTailInsert(LinkList& L) {
 //按序号查找结点
 LNode* GetElem(LinkList L, int i) {
     int j = 1;
+
     LNode* p = L->next;
 
     if (i == 0)
         return L;
     if (i < 0)
         return NULL;
-
     while (p && j < i) {
         p = p->next;
         j++;
@@ -84,16 +72,15 @@ LNode* GetElem(LinkList L, int i) {
     return p;
 }
 
-//按值查找
-LNode* LocateElem(LinkList L, ElemType e) {
+//按值查找结点
+LNode* LocateElem(LinkList& L, ElemType e) {
     LNode* p = L->next;
     while (p && p->data != e)
         p = p->next;
     return p;
 }
 
-//插入操作：插入到链表第i个位置
-// i是序号
+//将新结点插入第i个位置
 bool ListInsert(LinkList& L, int i, ElemType e) {
     LNode* p = GetElem(L, i - 1);
 
@@ -110,7 +97,7 @@ bool ListInsert(LinkList& L, int i, ElemType e) {
 }
 
 //删除第i个结点
-bool ListDel(LinkList& L, int i) {
+bool ListDelete(LinkList& L, int i) {
     LNode* p = GetElem(L, i - 1);
     LNode* q = p->next;
     p->next = q->next;
@@ -118,23 +105,52 @@ bool ListDel(LinkList& L, int i) {
     return true;
 }
 
+//打印函数
+void PrintList(LinkList L) {
+    L = L->next;
+    while (L) {
+        printf("%6d", L->data);
+        L = L->next;
+    }
+    printf("\n");
+}
+
 int main() {
     LinkList L;
-    // ListHeadInsert(L);
-    // PrintList(L);
 
+    //头插法
+    ListHeadInsert(L);
+    printf("头插法建立的单链表:");
+    PrintList(L);
+
+    //尾插法
     ListTailInsert(L);
+    printf("尾插法建立的单链表:");
     PrintList(L);
 
-    // LNode* p = GetElem(L, 2);
-    // printf("%d\n", p->data);
+    //按序号查找
+    LNode* p = GetElem(L, 2);
+    printf("单链表第2个元素是：%d\n", p->data);
 
-    // LNode* p = LocateElem(L, 5);
-    // printf("%d\n", p->data);
+    //按值查找
+    p = LocateElem(L, 10);
+    printf("单链表中值为10的元素是：%d\n", p->data);
 
-    ListInsert(L, 2, 10);
+    //新结点插入第i个位置
+    bool flag = ListInsert(L, 2, 100);
+    if (flag) {
+        printf("新结点值为100，插入后：");
+        PrintList(L);
+    } else
+        printf("插入失败\n");
+
+    //删除第i个位置
+    printf("第二个元素删除前的单链表如下：\n");
     PrintList(L);
-
-    ListDel(L, 2);
-    PrintList(L);
+    flag = ListDelete(L, 2);
+    if (flag) {
+        printf("第二个位置删除后的单链表如下：\n");
+        PrintList(L);
+    } else
+        printf("删除失败\n");
 }
---
+++ chapter7 search/6.2 顺序查找与折半查找/20220927.cpp
@@ -0,0 +1,14 @@
+/*
+ * @Author: 2361067080@qq.com
+ * @Date: 2022-09-27 17:18:24
+ * @Description: À≥–Ú≤È’“
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main() {
+
+
+}
